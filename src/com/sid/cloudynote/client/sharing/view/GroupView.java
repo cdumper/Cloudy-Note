b3e3f67a8bc97b6b447129a72104a7d2f0d17dde
sid <sidchina.ire@gmail.com>
2013-03-02 15:46:48 +0000

    Issue #57 #59 #60: Optimize ui of groups list in sharing view

--- src/com/sid/cloudynote/client/sharing/view/GroupView.java
+++ src/com/sid/cloudynote/client/sharing/view/GroupView.java
@@ -1,36 +1,58 @@
 package com.sid.cloudynote.client.sharing.view;
 
-import java.util.Arrays;
 import java.util.Set;
 
-import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.LIElement;
+import com.google.gwt.dom.client.UListElement;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.EventListener;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.SelectionChangeEvent;
-import com.google.gwt.view.client.SingleSelectionModel;
 import com.sid.cloudynote.client.event.GroupsChangedEvent;
 import com.sid.cloudynote.client.event.interfaces.IGroupsChangedHandler;
 import com.sid.cloudynote.client.sharing.view.interfaces.IGroupView;
 import com.sid.cloudynote.client.view.Container;
 import com.sid.cloudynote.shared.Group;
 
-public class GroupView extends Composite implements IGroupView, IGroupsChangedHandler{
+public class GroupView extends Composite implements IGroupView,
+		IGroupsChangedHandler {
 	@UiField
 	Container container;
+	@UiField
+	HTMLPanel content;
+	@UiField
+	Button publicButton;
+	@UiField
+	Button sharedButton;
+	@UiField
+	UListElement groupList;
+	@UiField
+	Style style;
+
+	interface Style extends CssResource {
+		String groupItem();
+
+		String groupLabel();
+
+		String button();
+	}
+
 	public Container getContainer() {
 		return container;
 	}
 
 	private Presenter presenter;
-	private String PUBLIC = "Public";
-	private String SHARED_WITH_ME = "Shared with me";
-	private String GROUP = "Group";
-	private CellList<String> cellList;
-	private SingleSelectionModel<String> selectionModel;
 
 	private static GroupViewUiBinder uiBinder = GWT
 			.create(GroupViewUiBinder.class);
@@ -38,46 +60,49 @@ public class GroupView extends Composite implements IGroupView, IGroupsChangedHa
 	interface GroupViewUiBinder extends UiBinder<Widget, GroupView> {
 	}
 
+	@UiHandler("publicButton")
+	void onPublicButtonClicked(ClickEvent e) {
+		presenter.viewPublic();
+	}
+
+	@UiHandler("sharedButton")
+	void onSharedButtonClicked(ClickEvent e) {
+		presenter.viewShared();
+	}
+
 	public GroupView() {
 		initWidget(uiBinder.createAndBindUi(this));
-		TextCell cell = new TextCell();
-		cellList = new CellList<String>(cell);
-		cellList.setRowData(Arrays.asList(PUBLIC,SHARED_WITH_ME,GROUP));
-		this.container.add(cellList);
-		selectionModel = new SingleSelectionModel<String>();
-		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler(){
-			@Override
-			public void onSelectionChange(SelectionChangeEvent event) {
-				// TODO Auto-generated method stub
-				if (PUBLIC.equals(selectionModel.getSelectedObject())){
-					presenter.viewPublic();
-				} else if (SHARED_WITH_ME.equals(selectionModel.getSelectedObject())){
-					presenter.viewShared();
-				} 
-//				else if (GROUP.equals(selectionModel.getSelectedObject())){
-//					presenter.viewGroups();
-//				}
-			}
-		});
-		cellList.setSelectionModel(selectionModel);
-//		selectionModel.setSelected(PUBLIC, true);
 	}
 
 	@Override
 	public void setPresenter(Presenter presenter) {
 		this.presenter = presenter;
 	}
-	
+
 	@Override
-	public Widget asWidget(){
-		return this.cellList;
+	public Widget asWidget() {
+		return this.content;
 	}
-	
-	@Override 
-	public void setGroupList (Set<Group> groups) {
+
+	@Override
+	public void setGroupList(Set<Group> groups) {
 		if (groups != null && groups.size() != 0) {
-			for (Group g : groups) {
-				System.out.println(g.getName());
+			while (groupList.hasChildNodes()) {
+				groupList.removeChild(groupList.getFirstChild());
+			}
+			for (final Group g : groups) {
+				LIElement li = Document.get().createLIElement();
+				Element button = DOM.createButton();
+				button.setInnerText(g.getName());
+				DOM.sinkEvents(button, Event.ONCLICK);
+				DOM.setEventListener(button, new EventListener() {
+					public void onBrowserEvent(Event event) {
+						//TODO show all notes in the group g.getName()
+					}
+				});
+				button.addClassName(style.groupItem());
+				li.appendChild(button);
+				groupList.appendChild(li);
 			}
 		}
 	}
